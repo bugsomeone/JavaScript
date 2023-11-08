126e4a118ac2f535b092408f76cb4379cba39190
Ace <richardsedlak2006@gmail.com>
2023-11-08 13:09:37 +0100

    Edit

--- II_TPI_01/Assets/app.js
+++ II_TPI_01/Assets/app.js
@@ -16,6 +16,7 @@ let vysledok01 = prvaPremena + druhaPremena;
 //150,2 / 8 * (18,3 - 20,1) / 3;
 //3**2 + 8 - (31/8);
 
+/**
 let a = 15 + 3 * (4 - 8 / 2);
 let b = ((150.2 / 8) * (18.3 - 20.1)) / 3;
 let c = 3 ** 2 + 8 - 31 / 8;
@@ -25,3 +26,15 @@ let prikaldC = "3 ** 2 + 8 - 31 / 8";
 alert(prikaldA + " = " + a);
 alert(prikaldB + " = " + b);
 alert(prikaldC + " = " + c);
+*/
+
+let mojVek = 17;
+let meno = 'Richard';
+let priezvisko = 'Sedlak';
+let mojeMeno = meno + "" + priezvisko;
+let ulica = 'Sasinková';
+let mesto = 'Kopčany';
+let PSČ = '908 48';
+let mojaAdresa =  ulica + " " + mesto + " " + PSČ;
+let adresaSkoly = 'Holíč 908 51, Námestie sv. Martina 5';
+alert(mojaAdresa);
